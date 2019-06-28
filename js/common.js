d687dc9707e4cde0259dd91c8359d7f9f9f9aacf
Nikolay <makarin@kgdnet.ru>
2019-06-28 20:49:51 +0200

    Finished template

--- js/common.js
+++ js/common.js
@@ -1,21 +1,52 @@
-$(function() {
+document.body.onload = function() {
+  setTimeout(function() {
+    var preloader = document.getElementById('page-preloader');
+    if( !preloader.classList.contains('done') )
+    {
+      preloader.classList.add('done');
+    }
+  }, 1000);
+}
 
-	// Custom JS
+$(document).ready(function() {
 
+$('.owl-carousel').owlCarousel({
+    items: 4,
+    dots: true,
+    autoplay: true,
+    autoHeight : true,
+    loop: true,
+    responsive : {
+            1 : { items : 1  },
+            767 : { items : 2  }, // from zero to 480 screen width 4 items
+            991 : { items : 3  }, // from 480 screen widthto 768 6 items
+            1199 : { items : 4   // from 768 screen width to 1024 8 items
+            }
+        },
 });
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:5,
-    loop:true,
-    margin: 10,
-    autoplay:true,
-    autoplayTimeout:3000,
-    autoplayHoverPause:true,
-    	responsive : {
-	    		1 : { items : 1  },
-	            767 : { items : 3  }, // from zero to 480 screen width 4 items
-	            991 : { items : 4  }, // from 480 screen widthto 768 6 items
-	            1199 : { items : 5}  // from 768 screen width to 1024 8 items
-	}
+   $("a").click(function() {
+      $("html, body").animate({
+         scrollTop: $($(this).attr("href")).offset().top+ "px"
+      }, {
+         duration: 1600, 
+         easing: "swing"
+      });
+      return false;
+   });
+
+   $(window).scroll(function() {
+        if($(this).scrollTop() != 0) { 
+            $('#toTop').fadeIn(); 
+        } else {
+            $('#toTop').fadeOut();
+        }
+ 
+    });
+ 
+    $('#toTop').click(function() {
+ 
+    $('body,html').animate({scrollTop:0},800);
+ 
+    });
 });
